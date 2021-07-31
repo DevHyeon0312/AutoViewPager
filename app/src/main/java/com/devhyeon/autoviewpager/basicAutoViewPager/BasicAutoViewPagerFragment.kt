9fe903ec754f157b65aa78faad166468e7eeafe0
devhyeon <devhyeon0312@gmail.com>
2021-08-01 01:09:52 +0900

    BasicAutoViewPagerFragment.kt - 드래그 중에는 Auto Stop

--- app/src/main/java/com/devhyeon/autoviewpager/basicAutoViewPager/BasicAutoViewPagerFragment.kt
+++ app/src/main/java/com/devhyeon/autoviewpager/basicAutoViewPager/BasicAutoViewPagerFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.widget.ViewPager2
 import com.devhyeon.autoviewpager.R
 import com.devhyeon.autoviewpager.databinding.FragmentAutoViewpagerBinding
 import com.google.android.material.tabs.TabLayoutMediator
@@ -34,6 +35,8 @@ class BasicAutoViewPagerFragment : Fragment() {
         private val TAG = "DevHyeon >>> " + BasicAutoViewPagerFragment::class.java.name
     }
 
+    var isAutoScroll: Boolean = true
+
     /**
      * Data Binding inflate will proceed here.
      * */
@@ -54,6 +57,8 @@ class BasicAutoViewPagerFragment : Fragment() {
 //            tab.setIcon(R.drawable.ic_launcher_foreground)    //Tab Icon
         }.attach()
 
+        binding.viewPager.registerOnPageChangeCallback(pageChangeCallback)
+
         return binding.root
     }
 
@@ -65,6 +70,36 @@ class BasicAutoViewPagerFragment : Fragment() {
         autoScrollViewPager()
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding.viewPager.unregisterOnPageChangeCallback(pageChangeCallback)
+    }
+
+    private val pageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {}
+        override fun onPageScrolled(
+            position: Int,
+            positionOffset: Float,
+            positionOffsetPixels: Int
+        ) {
+        }
+
+        override fun onPageScrollStateChanged(state: Int) {
+            when (state) {
+                ViewPager2.SCROLL_STATE_DRAGGING -> {
+                    // User begins dragging or a fake drag is started.
+                    isAutoScroll = false
+                }
+                ViewPager2.SCROLL_STATE_IDLE -> {
+                    // Pager is fully stopped/idle.
+                    isAutoScroll = true
+                }
+                ViewPager2.SCROLL_STATE_SETTLING -> {
+                    // Pager is automatically settling to the current page.
+                }
+            }
+        }
+    }
 
     /**
      * Move one PAGE for each AUTO_TIME.
@@ -74,10 +109,12 @@ class BasicAutoViewPagerFragment : Fragment() {
         viewLifecycleOwner.lifecycleScope.launch {
             while (true) {
                 delay(AUTO_TIME)
-                if (binding.viewPager.currentItem == END_PAGE_INDEX) {
-                    binding.viewPager.currentItem = START_PAGE_INDEX
-                } else {
-                    binding.viewPager.currentItem = binding.viewPager.currentItem + 1
+                if (isAutoScroll) {
+                    if (binding.viewPager.currentItem == END_PAGE_INDEX) {
+                        binding.viewPager.currentItem = START_PAGE_INDEX
+                    } else {
+                        binding.viewPager.currentItem = binding.viewPager.currentItem + 1
+                    }
                 }
             }
         }
